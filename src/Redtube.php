6158f0a5576779938d4d3c06c2dc9b47373376cb
aethletic <gorshkov34@gmail.com>
2020-01-11 15:05:02 +0400

    [feat] add new methods  🙌

--- src/Redtube.php
+++ src/Redtube.php
@@ -2,6 +2,15 @@
 
 namespace Aethletic\RedtubeApi;
 
+/**
+ * Redtube API Wrapper
+ *
+ * @package Redtube
+ * @author  aethletic <hello@h3ro.ru>
+ * @version 1.0.0
+ *
+ * @see     https://github.com/aethletic/redtube-api
+ */
 class Redtube
 {
     private const BASE_API_URL           = 'https://api.redtube.com/';
@@ -12,14 +21,17 @@ class Redtube
     private static $errorMessage = '';
 
     /**
-     * Get video by id.
-     * @param  integer $id Video indetificator.
-     * @param  string  $thumbsize Possible values are AVAILABLE_THUMB_FORMAT.
-     * @param  string  $output Possible values are AVAILABLE_OUTPUT_TYPE.
+     * Get video by id
+     *
+     * Retrieves additional information about specific video by $id parameter.
+     *
+     * @param  integer $id          Video indetificator.
+     * @param  string  $thumbsize   Thumb size, possible values are AVAILABLE_THUMB_FORMAT.
+     * @param  string  $output      Output type, possible values AVAILABLE_OUTPUT_TYPE
      *
      * @return array|string
      */
-    public static function getVideo($id = null, $thumbsize = 'all', $output = self::DEFAULT_OUTPUT_TYPE)
+    public static function getVideoById($id = null, $thumbsize = 'all', $output = self::DEFAULT_OUTPUT_TYPE)
     {
         if (self::$errorMessage = self::validate($id, '$id', ['is_null', 'is_numeric'])) {
             return self::$errorMessage;
@@ -42,15 +54,22 @@ class Redtube
             ]
         ), $output);
 
-        $response['video']['url_video'] = !is_array($videoUrl = self::getMp4($response['video']['embed_url'])) ? $videoUrl : null;
+        // if output xml
+        if (!is_array($response)) {
+            return $response;
+        }
+
+        $response['video']['url_video'] = !is_array($videoUrl = self::getVideoMp4($response['video']['embed_url'])) ? $videoUrl : null;
 
         return $response;
     }
 
     /**
-     * Get all categories.
+     * Get all categories
      *
-     * @param  string  $output Possible values AVAILABLE_OUTPUT_TYPE.
+     * Retrieves all available categories.
+     *
+     * @param  string  $output Output type, possible values AVAILABLE_OUTPUT_TYPE
      *
      * @return array|string
      */
@@ -69,14 +88,104 @@ class Redtube
     }
 
     /**
-     * Search video by filter.
+     * Get all tags
      *
-     * @param  array   $filter
-     * @param  string  $output Possible values AVAILABLE_OUTPUT_TYPE.
+     * Retrieves all tags available.
+     *
+     * @param  string  $output Output type, possible values AVAILABLE_OUTPUT_TYPE
      *
      * @return array|string
      */
-    public static function search($params = [], $output = self::DEFAULT_OUTPUT_TYPE)
+    public static function getTags($output = self::DEFAULT_OUTPUT_TYPE)
+    {
+        if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
+            return self::$errorMessage;
+        }
+
+        return self::execute(self::BASE_API_URL . '?' . http_build_query(
+            [
+                'data'      => 'redtube.Tags.getTagList',
+                'output'    => $output
+            ]
+        ), $output);
+    }
+
+    /**
+     * Get all pronstars
+     *
+     * Retrieves all pornstars available.
+     * If pass $isDetailedList (not NULL or FALSE), retrieves
+     * all pornstars available with details (page url and star's thumb).
+     *
+     * @param  mixed   $isDetailedList Detailed list
+     * @param  integer $page           Page of list
+     * @param  string  $output         Output type, possible values AVAILABLE_OUTPUT_TYPE
+     *
+     * @return array|string
+     */
+    public static function getStars($isDetailedList = null, $page = '1', $output = self::DEFAULT_OUTPUT_TYPE)
+    {
+        if (self::$errorMessage = self::validate($page, '$page', ['is_numeric'])) {
+            return self::$errorMessage;
+        }
+
+        if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
+            return self::$errorMessage;
+        }
+
+        $callMethod = $isDetailedList !== null && $isDetailedList !== false ? 'redtube.Stars.getStarDetailedList' : 'redtube.Stars.getStarList';
+
+        return self::execute(self::BASE_API_URL . '?' . http_build_query(
+            [
+                'data'      => $callMethod,
+                'page'      => $page,
+                'output'    => $output
+            ]
+        ), $output);
+    }
+
+    /**
+     * Get all deleted videos
+     *
+     * Retrieves all deleted videos.
+     *
+     * @param  integer $page   Page of list
+     * @param  string  $output Output type, possible values AVAILABLE_OUTPUT_TYPE
+     *
+     * @return array|string
+     */
+    public static function getDeletedVideos($page = '1', $output = self::DEFAULT_OUTPUT_TYPE)
+    {
+        if (self::$errorMessage = self::validate($page, '$page', ['is_numeric'])) {
+            return self::$errorMessage;
+        }
+
+        if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
+            return self::$errorMessage;
+        }
+
+        return self::execute(self::BASE_API_URL . '?' . http_build_query(
+            [
+                'data'      => 'redtube.Videos.getDeletedVideos',
+                'page'      => $page,
+                'output'    => $output
+            ]
+        ), $output);
+    }
+
+    /**
+     * Search video by filter
+     *
+     * Retrieves video list, can be filtered by multiple parameters,
+     * including the possibility to query the API for videos containing a specific
+     * string in the title or description.
+     *
+     * @param  array   $filter Filter params
+     * @param  string  $output Output type, possible values AVAILABLE_OUTPUT_TYPE
+     *
+     * @return array|string
+     */
+    public static function searchVideo($params = [], $output = self::DEFAULT_OUTPUT_TYPE)
     {
         if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
             return self::$errorMessage;
@@ -91,14 +200,89 @@ class Redtube
     }
 
     /**
-     * Get source video URL (*.mp4) from embed URL.
+     * Checks active video or deleted
+     *
+     * Retrieves state of a specific video specified by video_id parameter,
+     * which is useful in order to keep your embedded videos up to date.
+     *
+     * @param  integer $id      Video indetificator
+     * @param  string  $output  Output type, possible values AVAILABLE_OUTPUT_TYPE
+     *
+     * @return array|string
+     */
+    public static function isVideoActive($id = null, $output = self::DEFAULT_OUTPUT_TYPE)
+    {
+        if (self::$errorMessage = self::validate($id, '$id', ['is_null', 'is_numeric'])) {
+            return self::$errorMessage;
+        }
+
+        if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
+            return self::$errorMessage;
+        }
+
+        return self::execute(self::BASE_API_URL . '?' . http_build_query(
+            [
+                'data'      => 'redtube.Videos.isVideoActive',
+                'video_id'  => $id,
+                'output'    => $output
+            ]
+        ), $output);
+    }
+
+    /**
+     * Get embed video code & url by video id
+     *
+     * Retrieves embed code (BASE64 encoded) for specific video by video_id parameter,
+     * which is useful to automatically embed videos.
+     *
+     * @param  integer $id      Video indetificator
+     * @param  string  $output  Output type, possible values AVAILABLE_OUTPUT_TYPE
+     *
+     * @return array|string
+     */
+    public static function getVideoEmbed($id = null, $output = self::DEFAULT_OUTPUT_TYPE)
+    {
+        if (self::$errorMessage = self::validate($id, '$id', ['is_null', 'is_numeric'])) {
+            return self::$errorMessage;
+        }
+
+        if (self::$errorMessage = self::validate($output, '$output', ['is_null', 'is_output'])) {
+            return self::$errorMessage;
+        }
+
+        $response = self::execute(self::BASE_API_URL . '?' . http_build_query(
+            [
+                'data'      => 'redtube.Videos.getVideoEmbedCode',
+                'video_id'  => $id,
+                'output'    => $output
+            ]
+        ), $output);
+
+        // if output xml
+        if (!is_array($response)) {
+            return $response;
+        }
+
+        if (array_key_exists('code', $response)) {
+            return $response;
+        }
+
+        $newResponse['code'] = $response['embed']['code'];
+        $newResponse['embed_url'] = base64_decode($newResponse['code']);
+
+        return $newResponse;
+    }
+
+
+    /**
+     * Get source video URL (*.mp4) from embed URL
      *
      * @param  string  $embedUrl Embed URL (like: https://embed.redtube.com/?id=9999999)
-     * @param  string  $output Possible values AVAILABLE_OUTPUT_TYPE.
+     * @param  string  $output   Output type, possible values AVAILABLE_OUTPUT_TYPE
      *
      * @return string|array
      */
-    public static function getMp4($embedUrl = null, $output = self::DEFAULT_OUTPUT_TYPE)
+    public static function getVideoMp4($embedUrl = null, $output = self::DEFAULT_OUTPUT_TYPE)
     {
         if (self::$errorMessage = self::validate($embedUrl, '$embedUrl', ['is_null', 'is_url'])) {
             return self::$errorMessage;
